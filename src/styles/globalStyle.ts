426ae6675a35b0ac07a0b26ad433f87f8811744b
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-01 01:51:37 +0900

    chore: globalStyle 설정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,28 +1,49 @@
 import { css } from '@emotion/react';
 
+import theme from './theme';
+
 const globalStyle = css`
   * {
     margin: 0;
+
     padding: 0;
+
     box-sizing: border-box;
+
     font: inherit;
     color: inherit;
+
     flex-shrink: 0;
   }
-  body {
-    font-family: Noto Sans CJK KR, sans-serif;
-    letter-spacing: -0.03px;
+
+  html {
+    -webkit-tap-highlight-color: transparent;
   }
+
+  body {
+    letter-spacing: -0.025rem;
+
+    font-size: ${theme.size[16]};
+    font-weight: 400;
+    line-height: 1.5;
+    text-align: left;
+    color: ${theme.colors.black_1};
+  }
+
   a {
     text-decoration: none;
     color: inherit;
   }
+
   ol,
   ul {
     list-style: none;
   }
+
   button {
     cursor: pointer;
+    border: 0;
+    background-color: inherit;
   }
 `;
 
