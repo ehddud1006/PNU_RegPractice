38447301c00c4ff4fbecb4143017e787f0c6705a
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-02 03:01:04 +0900

    feat: PageTemplate 헤더 분기처리

--- src/@components/@shared/PageTemplate/PageTemplate.tsx
+++ src/@components/@shared/PageTemplate/PageTemplate.tsx
@@ -4,10 +4,16 @@ import Header from '@/@components/@shared/Header';
 
 import * as Styled from './PageTemplate.styles';
 
-const PageTemplate = (props: PropsWithChildren) => {
+type PageTemplateProps = {
+  isLogin?: boolean;
+};
+
+const PageTemplate = (props: PropsWithChildren<PageTemplateProps>) => {
+  const { isLogin = false } = props;
+
   return (
     <Styled.Root>
-      <Header />
+      {isLogin ? <Header.LoginHeader /> : <Header />}
       {props.children}
     </Styled.Root>
   );
