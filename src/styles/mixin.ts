3eabd89d7af9a1588ef02c76a618c0ff9b26540e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-02 03:02:38 +0900

    feat: a11y 추가

---
+++ src/styles/mixin.ts
@@ -0,0 +1,12 @@
+const hideWithA11y = `
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
+`;
+
+export { hideWithA11y };
